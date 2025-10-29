f486e0a049e895dfa2e7d380158b12e39ef41ca0
RedMund <BlackRedMund@mail.ru>
2025-10-29 12:35:56 +0300

    Добавлен декоратор проверки времени работы функции

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+import time
+def time_check(amount_time):
+    def inner(func):
+        def wrapper(*args, **kwargs):
+            time_start = time.perf_counter()
+            time.sleep(amount_time)
+            res = time_start - time.perf_counter()
+            print(res, func(*args, **kwargs))cd 
+            return func(*args, **kwargs)
+        return wrapper
+    return inner
+
+
+@time_check(5)
 def pow(x, y):
     res = x**y
-    print(res)
     return res
+
+
 
 pow(8, 3)
